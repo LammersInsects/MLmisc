2e2c43178be05401e3aa5be6ac50843aa98455a6
LammersInsects <marklammers6@gmail.com>
2021-01-11 16:52:08 +0100

    Added a check to normalize.date.R

--- normalize.date.R
+++ normalize.date.R
@@ -19,6 +19,9 @@
 
 # Define function
 normalize.date<-function(date.vector){
+  if(class(date.vector)!='Date'){
+    print('WARNING: This function is meant to normalize data towards the earliest date. Functionality with other vectors is untested!')
+  }
   norm.date.vector<-as.integer(date.vector-min(date.vector))
   return(norm.date.vector)
 }

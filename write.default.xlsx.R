a736691469e6165477c6b02c8525b9e3ad5dd2a8
LammersInsects <marklammers6@gmail.com>
2021-01-11 16:18:16 +0100

    Cleaned the code for the subtitle generation and included some checks

--- write.default.xlsx.R
+++ write.default.xlsx.R
@@ -28,6 +28,8 @@ write.default.xlsx<-function(dataframe,
   print('This function expects a any dataframe with a limited number of columns')
   print('Header is compulsory')
   
+  # Check input
+  #file name
   if(is.na(file)){
     file<-filename
     file.name<-rev(unlist(strsplit(filename, split = '/', fixed = T)))[1]
@@ -35,13 +37,29 @@ write.default.xlsx<-function(dataframe,
     file.name<-filename
   }
   
+  #column widths
+  #TODO #test what happens if no input is given presently
+  
+  #extra header info for the subtitle
+  if(!is.na(extra.header.info[1])){
+    if(length(extra.header.info)>1){
+      stop('ERROR: extra.header.info can only be a single string')
+    } else {
+      subtitle<-paste(extra.header.info, '  |   Last processed:',Sys.time())
+    }
+  } else {
+    subtitle<-paste('Last processed:',Sys.time())
+  }
+  
   # A full script to write a formatted xlsx file
   # See http://www.sthda.com/english/wiki/r-xlsx-package-a-quick-start-guide-to-manipulate-excel-files-in-r
   # and http://www.sthda.com/english/wiki/r2excel-read-write-and-format-easily-excel-files-using-r-software
   # Create a new workbook for outputs. Possible values for type are : "xls" and "xlsx"
   wb<-createWorkbook(type="xlsx")
+  
   # Define some cell styles
   #++++++++++++++++++++
+  
   # Title and sub title styles
   TITLE_STYLE <- CellStyle(wb)+ Font(wb,  heightInPoints=16, 
                                      #color="blue", 
@@ -49,14 +67,17 @@ write.default.xlsx<-function(dataframe,
   SUB_TITLE_STYLE <- CellStyle(wb) + 
     Font(wb,  heightInPoints=11, 
          isItalic=TRUE, isBold=FALSE)
+  
   # Styles for the data table row/column names
   TABLE_ROWNAMES_STYLE <- CellStyle(wb) + Font(wb, isBold=TRUE)
   TABLE_COLNAMES_STYLE <- CellStyle(wb) + Font(wb, isBold=TRUE) +
     Alignment(wrapText=TRUE, horizontal="ALIGN_CENTER") +
     Border(color="black", position=c("TOP", "BOTTOM"), 
-           pen=c("BORDER_THIN", "BORDER_THICK")) 
+           pen=c("BORDER_THIN", "BORDER_THICK"))
+  
   # Create a new sheet in the workbook
   sheet <- createSheet(wb, sheetName = file.name)
+  
   #++++++++++++++++++++++++
   # Helper function to add titles
   #++++++++++++++++++++++++
@@ -72,21 +93,24 @@ write.default.xlsx<-function(dataframe,
     setCellStyle(sheetTitle[[1,1]], titleStyle)
   }
   # Add title
-  xlsx.addTitle(sheet, rowIndex=1, title=file.name,
+  xlsx.addTitle(sheet, rowIndex=1, 
+                title=file.name,
                 titleStyle = TITLE_STYLE)
   # Add sub title
   xlsx.addTitle(sheet, rowIndex=2, 
-                title=paste(extra.header.info, '  |   Last processed:',Sys.time()),
+                title=subtitle,
                 titleStyle = SUB_TITLE_STYLE)
   # Add a table
   addDataFrame(dataframe, sheet, startRow=3, startColumn=1, row.names = FALSE,
                colnamesStyle = TABLE_COLNAMES_STYLE,showNA = F,
                rownamesStyle = TABLE_ROWNAMES_STYLE)
+  
   # Change column width
   widths<-colwidths
   for (i in (c(1:ncol(dataframe)))){
     setColumnWidth(sheet, colIndex=i, colWidth=widths[i])
   }
+  
   # Save the workbook to a file
   saveWorkbook(wb = wb, file = file)
   

17b573a7f97390f069be2e154760cd628cea44d9
LammersInsects <marklammers6@gmail.com>
2021-01-12 13:41:17 +0100

    Wrote a function to test for character encoding issues

---
+++ detect.encoding.issues.R
@@ -0,0 +1,41 @@
+# Written by Mark Lammers; Institute for Evolution and Biodiversity, University of Muenster; marklammers6@gmail.com
+# (c) 2021. Released under the terms of the GNU General Public License v3.
+
+# source("E:/DATA/Common_scripts/R_startup_script.R") #Dell using Maxtor HDD
+# source("D:/DATA/Common_scripts/R_startup_script.R") #Dell internal data
+
+# setwd(paste(wd.base, [DATA DRIVE SUBFOLDER], sep=''))
+
+# Load packages
+# if(!require('')){install.packages('')}
+# library('')
+
+# Load data 
+# df<-read.table('D:/DATA/Projects_home/Takenlijst/Database/20201203.Takenlijst.registry.backup', sep=';', header=T)
+# df[grep('Austr',df$Wat),]
+
+# Reformat data
+# for(i in 1:length(df$Wat)){
+#   to.test<-df$Wat[i]
+#   tt<-try(strsplit(to.test,nchar(to.test)))
+#   res<-ifelse(is(tt,'try-error'),"There was a warning or an error","OK")
+#   if(res!='OK'){
+#     print(paste(to.test, class(tt), res))
+#   }
+# }
+# x<-df$Wat
+# df[detect.encoding.issues(df$Wat),]
+
+# Define function
+detect.encoding.issues<-function(x){
+  if(class(x)!='character'){
+    print('WARNING: This function has only been tested on character vectors')
+  } 
+  unlist(lapply(x, function(i){
+    #try to count number of characters of the string, it will fail if there is an encoding issue, giving class 'try-error'
+    tt<-class(try(nchar(i), silent = T))=='try-error' 
+    return(tt)
+  }))
+}
+
+# Explore and plot data

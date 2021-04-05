e7c911995060028b7582ace33accd16f8666c389
LammersInsects <marklammers6@gmail.com>
2021-04-05 12:39:23 +0200

    Print output of time.passed() in same precision as input

--- time.passed.R
+++ time.passed.R
@@ -14,17 +14,32 @@
 
 # Define function
 time.passed<-function(start, end, return.it=F){ #start and end should be timestamps as produced by now()
-  if(nchar(start)<20){ #microsecond precision has 20 characters
-    start<-paste(start,paste(rep(0,20-nchar(start)),collapse=''),sep='') #add as many zeros as required
+  start.n<-nchar(start)
+  end.n<-nchar(end)
+  if(min(start.n,end.n)<14){
+    stop('ERROR: Number of characters of input indicates precision is above the seconds. Is the input produced by MLmisc::now()?')
+  } else if(min(start.n,end.n)==14){ #Precision should be in seconds
+    
+  } else if(min(start.n,end.n)>14){ #Precision in decimal seconds
+    
   }
-  if(nchar(end)<20){ #microsecond precision has 20 characters
-    end<-paste(end,paste(rep(0,20-nchar(end)),collapse=''),sep='') #add as many zeros as required
-  }
+  
+  #default for seconds precision
   tp<-difftime(strptime(x = end, format = '%Y%m%d%H%M%S'), strptime(x = start, format = '%Y%m%d%H%M%S'))
+  
+  if(start.n<20){ #microsecond precision has 20 characters
+    start<-paste(start,paste(rep(0,20-start.n),collapse=''),sep='') #add as many zeros as required
+  }
+  if(end.n<20){ #microsecond precision has 20 characters
+    end<-paste(end,paste(rep(0,20-end.n),collapse=''),sep='') #add as many zeros as required
+  }
+  
   if((as.numeric(end)-as.numeric(start))<60000000){ #when something takes less than a minute,
     tp<-(as.numeric(end)-as.numeric(start))/1000000 #report output in decimal seconds
+    tp<-round(tp,digits=min(start.n,end.n)-14) #strip any decimals that are below the input precision
     tp<-as.difftime(tp, units = 'secs')
-  }
+  } #else use default difftime
+  
   print(paste('Time passed:',format(tp)))
   if(return.it){
     return(tp)

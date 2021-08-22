6db8317437d1440df1e0c642a0b2a1dd119c0c49
LammersInsects <marklammers6@gmail.com>
2021-08-22 14:25:11 +0200

    Add script to check whether an output file has been successfully updated

---
+++ is.file.updated.R
@@ -0,0 +1,29 @@
+# Written by Mark Lammers; Institute for Evolution and Biodiversity, University of Muenster; marklammers6@gmail.com
+# (c) 2021. Released under the terms of the GNU General Public License v3.
+
+# Load packages
+# if(!require('')){install.packages('')}
+# library('')
+
+# Define function
+is.file.updated<-function(full.file.path){
+  
+  #does the file exist at all?
+  if(!file.exists(full.file.path)){
+    stop('File does not exist!')
+  }
+  
+  #is the file actually a folder?
+  if(file.info(full.file.path)$isdir){
+    stop('File is a folder!')
+  }
+  
+  #is the file empty?
+  if(!file.info(full.file.path)$size>0){
+    stop('File is empty!')
+  }
+  
+  #check date and time: less than 5 minutes old?
+  
+  return()
+}

c62863c9532104e6b802e820dedab83086acb3fe
LammersInsects <marklammers6@gmail.com>
2021-08-24 14:56:05 +0200

    Do not use errors and return TRUE or FALSE as output in is.file.updated.R

--- is.file.updated.R
+++ is.file.updated.R
@@ -6,27 +6,33 @@
 # library('')
 
 # Define function
-is.file.updated<-function(full.file.path){
+is.file.updated<-function(full.file.path, seconds.before.now=300){
+  
+  #output is alwways true except when any of the tests fails
+  output<-T
   
   #does the file exist at all?
   if(!file.exists(full.file.path)){
-    stop('File does not exist!')
+    cat('File does not exist!\n')
+    output<-F
   }
   
   #is the file actually a folder?
   if(file.info(full.file.path)$isdir){
-    stop('File is a folder!')
+    cat('WARNING: File is a folder!\n')
   }
   
   #is the file empty?
   if(!file.info(full.file.path)$size>0){
-    stop('File is empty!')
+    cat('File is empty!\n')
+    output<-F
   }
   
-  #check date and time: less than 5 minutes old?
-  if(!file.info(full.file.path)$mtime > (Sys.time() - 300)){
-    stop("The file's last modification is more than 5 minutes ago!")
+  #check date and time: less than a given number of seconds old?
+  if(!file.info(full.file.path)$mtime > (Sys.time() - seconds.before.now)){
+    cat("The file's last modification is more than ",seconds.before.now," seconds ago!",sep='',"\n")
+    output<-F
   }
   
-  return()
+  return(output)
 }

4df7a00a8940aa27bfa8d3e3ade4d0325bec8347
LammersInsects <marklammers6@gmail.com>
2021-08-31 21:33:31 +0200

    Add function havingIP.R for testing the internet connection

---
+++ havingIP.R
@@ -0,0 +1,18 @@
+# Written by Mark Lammers; Institute for Evolution and Biodiversity, University of Muenster; marklammers6@gmail.com
+# (c) 2021. Released under the terms of the GNU General Public License v3.
+
+# Load packages
+# if(!require('')){install.packages('')}
+# library('')
+
+# Define function
+#Solution by eyjo at https://stackoverflow.com/questions/5076593/how-to-determine-if-you-have-an-internet-connection-in-r
+havingIP <- function() {
+  if (.Platform$OS.type == "windows") {
+    ipmessage <- system("ipconfig", intern = TRUE)
+  } else {
+    ipmessage <- system("ifconfig", intern = TRUE)
+  }
+  validIP <- "((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)[.]){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
+  any(grep(validIP, ipmessage))
+}

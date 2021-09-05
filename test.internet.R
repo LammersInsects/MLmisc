3d3b187167c5eea498cff9c048b372fea42e5d68
LammersInsects <marklammers6@gmail.com>
2021-09-05 11:49:13 +0200

    Make function test.internet.R work on computers without curl installed

--- test.internet.R
+++ test.internet.R
@@ -7,6 +7,8 @@
 
 # Define function
 #Solution by eyjo at https://stackoverflow.com/questions/5076593/how-to-determine-if-you-have-an-internet-connection-in-r
+#also include all other given solutions from that page
+
 havingIP <- function(){
   if (.Platform$OS.type == "windows") {
     ipmessage <- system("ipconfig", intern = TRUE)
@@ -14,9 +16,22 @@ havingIP <- function(){
     ipmessage <- system("ifconfig", intern = TRUE)
   }
   validIP <- "((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)[.]){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
-  any(grep(validIP, ipmessage))
+  any(grep(validIP, ipmessage[grep("127.0.0.1", ipmessage, invert = T)]))
+}
+
+is_online <- function(site="http://example.com/") {
+  tryCatch({
+    readLines(site,n=1)
+    TRUE
+  },
+  warning = function(w) invokeRestart("muffleWarning"),
+  error = function(e) FALSE)
 }
 
 test.internet<-function(){
-  havingIP() & curl::has_internet()
+  if(!require(curl)){
+    havingIP() & is_online()
+  } else {
+    havingIP() & curl::has_internet()
+  }
 }

246151a43f35c2cad4c2616daaf1e5b026e4e54a
LammersInsects <marklammers6@gmail.com>
2021-11-15 13:01:08 +0100

    New way of parsing https to read scripts from the web for source_https()

--- github2R.R
+++ github2R.R
@@ -1,16 +1,21 @@
 # https://www.r-bloggers.com/source_https-sourcing-an-r-script-from-github-over-https/
 # November 24, 2011
 # By Tony Breyal
+# revised version: https://stackoverflow.com/questions/35720660/how-to-use-an-r-script-from-github
+# and getURL error fixed with https://stackoverflow.com/questions/21237469/r-specify-ssl-version-in-rcurl-geturl-statement
 
 # install.packages('RCurl')
-source_https <- function(url, ...) {
+source_https <- function(URL, unlink.tmp.certs = FALSE) {
   # load package
   require(RCurl)
   
-  # parse and evaluate each .R script
-  sapply(c(url, ...), function(u) {
-    eval(parse(text = getURL(u, followlocation = TRUE, cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))), envir = .GlobalEnv)
-  })
+  # read script lines from website using a security certificate
+  if(!file.exists("cacert.pem")) download.file(url="http://curl.haxx.se/ca/cacert.pem", destfile = "cacert.pem")
+  script <- getURL(URL, followlocation = TRUE, cainfo = "cacert.pem", ssl.verifypeer=TRUE, sslversion=4L)
+  if(unlink.tmp.certs) unlink("cacert.pem")
+  
+  # parase lines and evealuate in the global environement
+  eval(parse(text = script), envir= .GlobalEnv)
 }
 
 # Example
@@ -19,3 +24,25 @@ source_https <- function(url, ...) {
 
 # source_https("https://raw.githubusercontent.com/LammersInsects/R_magic/master/impact2whisky.R")
 # rm(impact2whisky)
+
+github2R<-function(github.full.link){
+  #test whether it's a https URL
+  if(!substr(github.full.link, 1, 5)=='https'){
+    cat('The link should start with <https> !')
+  }
+  
+  #convert the link to the raw page link
+  result<-gsub('github.com','raw.githubusercontent.com',github.full.link)
+  result<-gsub('blob/','',result)
+  
+  #load the script into R
+  source_https(result)
+  
+  #check success
+  #TODO
+}
+  
+#Example
+# github.full.link<-'https://github.com/LammersInsects/MLmisc/blob/main/duplicates.R'
+github2R(github.full.link)
+#result should be 'https://raw.githubusercontent.com/LammersInsects/MLmisc/main/duplicates.R'

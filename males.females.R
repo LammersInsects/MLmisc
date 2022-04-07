da32857f09d209fd5250e0677ff09df782063177
LammersInsects <marklammers6@gmail.com>
2022-04-07 17:16:16 +0200

    Update test data and write new format tests for males.females.R

--- males.females.R
+++ males.females.R
@@ -6,8 +6,8 @@
 # library('')
 
 # Load test data
-# xxmxxf<-c("169m47f", NA, "21m4f", "182m63f", "2m", "15f", "105m18f", NA, "169m60f", "[many]", "0m24f", "0m0f")
-# males.females(xxmxxf = xxmxxf)
+xxmxxf<-c("169m47f", "21m4f", "2m63f", "2m", "15f", "105m18f60u", "169m60u", '14f5u', '18u', NA, "[many]", '5m0f',"0m24f", "0m0f")
+males.females(xxmxxf = xxmxxf)
 
 # Reformat data
 
@@ -19,15 +19,24 @@ males.females<-function(xxmxxf){
     print(paste('WARNING:',test,'input values are NA'))
   }
   
-  #check if the values are of format xxmxxf
+  #check if the values are of format xxmxxfxxu
   broken<-sapply(xxmxxf[!emptyvalues(xxmxxf)],elements,n=1)
-  check<-rbind(sapply(sapply(broken,`%in%`,c('m')),sum)==1, #check whether the occurrence of m in string is 1
-               sapply(sapply(broken,`%in%`,c('f')),sum)==1) #same for f
-  if(sum(colSums(check)!=2)>0){
-    print(paste('WARNING:',sum(colSums(check)!=2),'input value do not specify males and females in format "xxmxxf":'))
-    print(xxmxxf[!is.na(xxmxxf)][colSums(check)!=2])
+  check<-data.frame(m=sapply(sapply(broken,`%in%`,c('m')),sum), #check whether the occurrence of m in string is 1
+                    f=sapply(sapply(broken,`%in%`,c('f')),sum), #same for f
+                    u=sapply(sapply(broken,`%in%`,c('u')),sum), #same for u
+                    num=sapply(sapply(broken,`%in%`,0:9),sum), #and for numbers
+                    other=sapply(sapply(broken,`%in%`,c(letters,LETTERS)[-c(6,13,21)]),sum), #also check for other letters
+                    len=sapply(broken, length)) #length of the strings
+  check$test.mfu<-check$m<=1 & check$f<=1 & check$u<=1
+  check$test.other<-check$other ==0
+  check$test.len<-check$len-check$num-check$m-check$f-check$u ==0
+  check$result<-rowSums(check[,grep('test',colnames(check))])!=3
+  if(sum(check$result)>0){
+    print(paste('WARNING:',sum(check$result),'input value(s) do not specify males and females in format [xxmxxfxxu]:'))
+    print(xxmxxf[!is.na(xxmxxf)][check$result])
   }
   
+  #extract numbers of males, females and unknown
   males<-as.integer(sapply(strsplit(xxmxxf, 'm'),head,n=1)) #gives a warning message if the string doesn't have an 'm'
   print('If this gives a warning about introduced NAs, check input format. You should already have been warned.')
   females<-sapply(strsplit(xxmxxf, 'm'),tail,n=1)
